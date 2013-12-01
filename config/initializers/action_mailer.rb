f4532473ed95c9850e7108b57d09d53821dfc107
Owen Davies <git@obduk.com>
2013-12-01 22:15:07 +0000

    Added config for SMTP_ADDRESS and SMTP_PORT

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
+  address: ENV['SMTP_ADDRESS'],
+  port: ENV['SMTP_PORT'].to_i,
   authentication: :plain,
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
