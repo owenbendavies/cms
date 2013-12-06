18fc23b930c51bbb5acdab6bdd4846d55f838de2
Owen Davies <git@obduk.com>
2013-12-06 20:40:07 +0000

    Fixed i18n deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module Cms
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     config.lograge.custom_options = lambda do |event|
       {
