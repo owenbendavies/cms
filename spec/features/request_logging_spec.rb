efec2555ba54bc74ed90b9cb160046da25a0b4d9
Owen Davies <git@obduk.com>
2013-12-08 16:55:48 +0000

    Added account_id to logs

--- spec/features/request_logging_spec.rb
+++ spec/features/request_logging_spec.rb
@@ -28,6 +28,7 @@ describe 'request logging' do
     events.first.payload[:remote_ip].should eq '127.0.0.1'
     events.first.payload[:request_id].should eq new_id
     events.first.payload[:user_agent].should eq new_company_name
+    events.first.payload[:account_id].should be_nil
   end
 
   it 'uses extra information in lograge' do
@@ -39,5 +40,20 @@ describe 'request logging' do
       request_id: new_id,
       user_agent: "\"#{new_company_name}\"",
     })
+  end
+
+  it_behaves_like 'logged in account' do
+    it 'logs account_id' do
+      events.last.payload[:account_id].should eq @account.id
+
+      result = Cms::Application.config.lograge.custom_options.call(events.last)
+      result.should eq ({
+        host: 'localhost',
+        remote_ip: '127.0.0.1',
+        request_id: new_id,
+        user_agent: "\"#{new_company_name}\"",
+        account_id: @account.id,
+      })
+    end
   end
 end
