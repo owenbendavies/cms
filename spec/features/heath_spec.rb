df4619d16d3378d3711b8041517c6ca53edeb7b6
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-01-25 21:51:19 +0000

    Fixed health check for unknown formats

--- spec/features/heath_spec.rb
+++ spec/features/heath_spec.rb
@@ -1,8 +1,31 @@
 require 'spec_helper'
 
 describe 'health' do
-  it 'renders ok' do
-    visit_page '/health'
-    page.should have_content 'ok'
+  context 'unknwon site' do
+    it 'renders ok' do
+      visit_page '/health.txt'
+      page.should have_content 'ok'
+    end
+
+    it 'renders site not found when not txt' do
+      visit '/health.xml'
+      page.status_code.should eq 404
+      page.should have_content 'Site Not Found'
+    end
+  end
+
+  context 'known site' do
+    include_context 'default_site'
+
+    it 'renders ok' do
+      visit_page '/health.txt'
+      page.should have_content 'ok'
+    end
+
+    it 'renders page not found when not txt' do
+      visit '/health'
+      page.status_code.should eq 404
+      page.should have_content 'Page Not Found'
+    end
   end
 end
