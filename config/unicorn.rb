0f19a021b6815697de8cc41c2276b83a9fbd3e3b
Owen Davies <git@obduk.com>
2014-02-03 04:16:00 +0000

    Don't kill old unicorn process

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 require 'syslogger'
-logger Syslogger.new("cms")
+logger Syslogger.new('cms')
 
 worker_processes 4
 
@@ -11,18 +11,6 @@ DEPLOY_PATH = File.expand_path('../..', __FILE__)
 
 working_directory DEPLOY_PATH
 
-PID_FILE = File.join(DEPLOY_PATH, "/tmp/pids/unicorn.pid")
-OLD_PID_FILE = PID_FILE + ".oldbin"
+pid File.join(DEPLOY_PATH, '/tmp/pids/unicorn.pid')
 
-pid PID_FILE
-
-listen File.join(DEPLOY_PATH, "/tmp/sockets/unicorn.sock")
-
-before_fork do |server, worker|
-  if File.exists?(OLD_PID_FILE)
-    begin
-      Process.kill("QUIT", File.read(OLD_PID_FILE).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
+listen File.join(DEPLOY_PATH, '/tmp/sockets/unicorn.sock')
