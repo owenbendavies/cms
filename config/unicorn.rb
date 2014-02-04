67066496e9745dc3445e366a69ba60b3546f4624
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-02-04 21:43:49 +0000

    Revert "Don't kill old unicorn process"
    This reverts commit 0f19a021b6815697de8cc41c2276b83a9fbd3e3b.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 require 'syslogger'
-logger Syslogger.new('cms')
+logger Syslogger.new("cms")
 
 worker_processes 4
 
@@ -11,6 +11,18 @@ DEPLOY_PATH = File.expand_path('../..', __FILE__)
 
 working_directory DEPLOY_PATH
 
-pid File.join(DEPLOY_PATH, '/tmp/pids/unicorn.pid')
+PID_FILE = File.join(DEPLOY_PATH, "/tmp/pids/unicorn.pid")
+OLD_PID_FILE = PID_FILE + ".oldbin"
 
-listen File.join(DEPLOY_PATH, '/tmp/sockets/unicorn.sock')
+pid PID_FILE
+
+listen File.join(DEPLOY_PATH, "/tmp/sockets/unicorn.sock")
+
+before_fork do |server, worker|
+  if File.exists?(OLD_PID_FILE)
+    begin
+      Process.kill("QUIT", File.read(OLD_PID_FILE).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
