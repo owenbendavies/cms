29dc5ed138b891cc977ee85510b7b6e9aab72ec8
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-08-04 10:40:23 +0100

    Improved message_mailer_spec

--- spec/mailers/message_mailer_spec.rb
+++ spec/mailers/message_mailer_spec.rb
@@ -9,54 +9,62 @@ describe MessageMailer do
 
     before do
       @account = FactoryGirl.create(:account)
+    end
 
+    it 'sends a message' do
       expect {
         MessageMailer.new_message(message).deliver
       }.to change{ActionMailer::Base.deliveries.size}.by(1)
     end
 
-    subject { ActionMailer::Base.deliveries.last }
+    describe 'from address' do
+      context 'site without www in host' do
+        it 'has from address of site' do
+          MessageMailer.new_message(message).deliver
+          subject = ActionMailer::Base.deliveries.last
 
-    it 'has from address of site' do
-      expect(subject.header["From"].to_s.gsub('"', '')).
-        to eq "#{site.name} <noreply@#{site.host}>"
+          expect(subject.header["From"].to_s.gsub('"', '')).
+            to eq "#{site.name} <noreply@#{site.host}>"
+        end
+      end
+
+      context 'site with www in host' do
+        it 'removes the www' do
+          site = FactoryGirl.build(:site, host: 'www.example.com')
+          message = FactoryGirl.build(:message, site: site)
+          FactoryGirl.create(:account, sites: ['www.example.com'])
+          MessageMailer.new_message(message).deliver
+          subject = ActionMailer::Base.deliveries.last
+
+          expect(subject.header["From"].to_s.gsub('"', '')).
+            to eq "#{site.name} <noreply@example.com>"
+        end
+      end
     end
 
     it 'is sent to account email address' do
+      MessageMailer.new_message(message).deliver
+      subject = ActionMailer::Base.deliveries.last
+
       expect(subject.to).to eq [@account.email]
     end
 
     it 'includes message subject' do
+      MessageMailer.new_message(message).deliver
+      subject = ActionMailer::Base.deliveries.last
+
       expect(subject.subject).to eq message.subject
     end
 
     it 'has a message body' do
+      MessageMailer.new_message(message).deliver
+      subject = ActionMailer::Base.deliveries.last
       body = subject.body
 
       expect(body).to include "Name: #{message.name}"
       expect(body).to include "Email address: #{message.email_address}"
       expect(body).to include "Phone number: #{message.phone_number}"
       expect(body).to include "Message: #{message.message}"
-    end
-  end
-
-  describe 'site with www host' do
-    let(:site) { FactoryGirl.build(:site, host: 'www.example.com') }
-    let(:message) { FactoryGirl.build(:message, site: site) }
-
-    before do
-      @account = FactoryGirl.create(:account, sites: ['www.example.com'])
-
-      expect {
-        MessageMailer.new_message(message).deliver
-      }.to change{ActionMailer::Base.deliveries.size}.by(1)
-    end
-
-    subject { ActionMailer::Base.deliveries.last }
-
-    it 'removes www from email' do
-      expect(subject.header["From"].to_s.gsub('"', '')).
-        to eq "#{site.name} <noreply@example.com>"
     end
   end
 end
