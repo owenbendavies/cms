165964a0264c5d55a658d55dba3179b53898b502
Owen Davies <git@obduk.com>
2014-08-09 17:25:29 +0100

    Use compiled assets in test mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,6 +16,19 @@ Cms::Application.configure do
   config.serve_static_assets  = true
   config.static_cache_control = "public, max-age=3600"
 
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Generate digests for assets URLs.
+  config.assets.digest = true
+
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version = '1.0'
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
