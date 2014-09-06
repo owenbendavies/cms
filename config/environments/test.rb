fdce73b66148e6791c49b53c51a242a62190097a
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-09-06 09:59:26 +0100

    Upgraded asset version

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,7 @@ Cms::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
