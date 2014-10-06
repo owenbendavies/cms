4a9d6dc798d1dcfa688dda42d2c0d81c0ef2a8bb
Owen Davies <git@obduk.com>
2014-10-06 17:46:00 +0100

    Removed Heroku specific environment variables

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address: ENV['SMTP_ADDRESS'],
   port: ENV['SMTP_PORT'].to_i,
   authentication: :plain,
-  user_name: (ENV['SMTP_USERNAME'] || ENV['SENDGRID_USERNAME']),
-  password: (ENV['SMTP_PASSWORD'] || ENV['SENDGRID_PASSWORD']),
+  user_name: ENV['SMTP_USERNAME'],
+  password: ENV['SMTP_PASSWORD'],
   domain: `hostname -f`.strip,
 }
