3eabdbcc2d7855b4735be8582c36cc1ad6c5c5c3
Owen Davies <git@obduk.com>
2014-10-25 13:56:38 +0100

    Change to use unicorn-rails for development

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,3 @@
-require 'syslogger'
-logger Syslogger.new("cms")
-
-worker_processes Integer(ENV['WORKER_PROCESSES'])
+worker_processes 3
 
 timeout 5
-
-preload_app true
-
-DEPLOY_PATH = File.expand_path('../..', __FILE__)
-
-working_directory DEPLOY_PATH
-
-PID_FILE = File.join(DEPLOY_PATH, "/tmp/pids/unicorn.pid")
-OLD_PID_FILE = PID_FILE + ".oldbin"
-
-pid PID_FILE
-
-listen File.join(DEPLOY_PATH, "/tmp/sockets/unicorn.sock")
-
-before_fork do |server, worker|
-  if File.exists?(OLD_PID_FILE)
-    begin
-      Process.kill("QUIT", File.read(OLD_PID_FILE).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
