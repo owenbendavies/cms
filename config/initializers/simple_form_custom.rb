51a728f0e65fde971f027340c3df551889aa71b4
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-11-08 15:31:44 +0000

    Fixed simple form

--- config/initializers/simple_form_custom.rb
+++ config/initializers/simple_form_custom.rb
@@ -1,5 +1,31 @@
 SimpleForm.setup do |config|
-  config.form_class = 'form-horizontal'
-  config.button_class = 'btn btn-primary'
+  config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.wrapper tag: 'div', class: 'col-sm-4 col-md-3' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  config.wrappers :wide, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.wrapper tag: 'div', class: 'col-sm-9 col-md-8' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+  config.default_wrapper = :bootstrap3
+
   config.browser_validations = true
+  config.button_class = 'btn btn-primary'
+  config.form_class = 'form-horizontal'
+  config.input_class = 'form-control'
+  config.label_class = 'control-label col-sm-3 col-md-2'
 end
