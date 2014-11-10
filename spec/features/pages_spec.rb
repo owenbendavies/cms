8ffe5e892fa27df8d3fbab3cfd8d86a7bffc8275
Owen Davies <obduk@users.noreply.github.com>
2014-11-10 12:33:36 +0000

    Fixed pages_spec

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -237,12 +237,12 @@ RSpec.describe 'pages', type: :feature do
 
           expect {
             click_link 'Delete'
-            expect(current_path).to eq '/sitemap'
+            expect(page.body).to include('Test Page was deleted')
           }.to change(Page, :count).by(-1)
         end
 
         it_should_have_alert_with 'Test Page was deleted'
-
+        expect(current_path).to eq '/sitemap'
         expect(Page.find_by_site_and_url(@site, 'test_page')).to be_nil
       end
 
