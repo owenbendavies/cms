8ba0f1dafbee8a741dc64fca93e40bf8524e75c0
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-11-12 14:24:01 +0000

    Validate password strength of accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,7 +15,9 @@ class Account
   property :password_digest
   has_secure_password
 
-  validates :password, length: {minimum: 8, maximum: 64, allow_blank: true}
+  validates :password,
+    length: {minimum: 8, maximum: 64, allow_blank: true},
+    password_strength: true
 
   validates :email, presence: true, length: {maximum: 64}, email_format: true
 
