211443040f43366bd31e05df8d6fd07528a01e9c
Owen Davies <obduk@users.noreply.github.com>
2014-11-13 14:43:26 +0000

    Made test passwords more secure to fix randomly failing tests

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :account do
     email { Faker::Internet.safe_email }
     sites ['localhost']
-    password { Faker::Lorem.words(4).join }
+    password { 'Aq2%' + Faker::Lorem.words(3).join }
     password_confirmation { password }
   end
 end
