1c175487aecb7ffd15c3f22bd8a36e9285ff4681
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-11-16 18:09:14 +0000

    Removed unneeded to_s in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ if Rails.application.secrets.redis_server
   Rails.application.config.session_store :redis_store,
     expire_after: 30.days,
     key: '_cms_session',
-    redis_server: Rails.application.secrets.redis_server.to_s + ':session'
+    redis_server: Rails.application.secrets.redis_server + ':session'
 end
