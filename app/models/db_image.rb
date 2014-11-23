3c7a60ae79ba456326c01c39d1b5f65923f11886
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-11-23 04:12:37 +0000

    Added db models

---
+++ app/models/db_image.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id            :integer          not null, primary key
+#  site_id       :integer          not null
+#  name          :string(64)       not null
+#  filename      :string(255)      not null
+#  created_by_id :integer          not null
+#  updated_by_id :integer          not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class DBImage < ActiveRecord::Base
+  self.table_name = 'images'
+
+  belongs_to :site, class_name: 'DBSite'
+  belongs_to :created_by, class_name: 'DBAccount'
+  belongs_to :updated_by, class_name: 'DBAccount'
+end
---
+++ app/models/db_page.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id            :integer          not null, primary key
+#  site_id       :integer          not null
+#  url           :string(64)       not null
+#  name          :string(64)       not null
+#  private       :boolean          default(FALSE)
+#  contact_form  :boolean          default(FALSE)
+#  html_content  :text
+#  created_by_id :integer          not null
+#  updated_by_id :integer          not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class DBPage < ActiveRecord::Base
+  self.table_name = 'pages'
+
+  belongs_to :site, class_name: 'DBSite'
+  belongs_to :created_by, class_name: 'DBAccount'
+  belongs_to :updated_by, class_name: 'DBAccount'
+end
