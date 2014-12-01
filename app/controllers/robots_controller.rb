c268ba1fc50a3bd04e56b21527ccf505adc0811f
Owen Davies <obduk@users.noreply.github.com>
2014-12-01 20:53:20 +0000

    Removed allow_search_engines option

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -2,10 +2,5 @@ class RobotsController < ApplicationController
   skip_before_filter :login_required
 
   def show
-    if @site.allow_search_engines
-      render :show
-    else
-      render 'robots/disallowed.txt'
-    end
   end
 end
