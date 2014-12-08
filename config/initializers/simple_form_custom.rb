b5c61ca43eba66ff338a131ea1901f7cf1c1ef17
Owen Davies <obduk@users.noreply.github.com>
2014-12-08 13:25:41 +0000

    Upgraded simple_form config

--- config/initializers/simple_form_custom.rb
+++ config/initializers/simple_form_custom.rb
@@ -2,7 +2,12 @@ SimpleForm.setup do |config|
   config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
     b.use :label
+
     b.wrapper tag: 'div', class: 'col-control' do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
@@ -13,7 +18,12 @@ SimpleForm.setup do |config|
   config.wrappers :wide, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
     b.use :label
+
     b.wrapper tag: 'div', class: 'col-control-wide' do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
@@ -23,10 +33,15 @@ SimpleForm.setup do |config|
 
   config.default_wrapper = :bootstrap3
 
+  config.wrapper_mappings = {
+    text: :wide,
+  }
+
   config.boolean_style = :inline
   config.browser_validations = true
   config.button_class = 'btn btn-primary'
   config.form_class = 'form-horizontal'
   config.input_class = 'form-control'
   config.label_class = 'control-label col-control-label'
+  config.error_notification_class = 'alert alert-error'
 end
