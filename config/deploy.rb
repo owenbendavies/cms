b74a16da588ed73bfa65635c003fe6ae4f70d7f2
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-12-09 07:25:36 +0000

    Added log to shared dirs in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:obduk/cms.git'
 
 set :deploy_to, '/var/www/cms'
 
-set :linked_dirs, ['tmp/pids', 'tmp/sockets']
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/sockets']
 
 set :log_level, :info
 
