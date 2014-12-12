e1c0b5aa6a675b64d1b6f0131536239a7f4584e4
Owen Davies <obduk@users.noreply.github.com>
2014-12-12 13:23:10 +0000

    Fixed rubocop errors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.application.secrets.redis_server
-  Rails.application.config.session_store :redis_store,
+  Rails.application.config.session_store(
+    :redis_store,
     expire_after: 30.days,
     key: '_cms_session',
     redis_server: Rails.application.secrets.redis_server + ':session'
+  )
 end
