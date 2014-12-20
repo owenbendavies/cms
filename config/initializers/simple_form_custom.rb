ec043494f229c2786d48e26010754c21b00081e9
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2014-12-20 22:59:20 +0000

    Fix deprecation warnings in simple_form

--- config/initializers/simple_form_custom.rb
+++ config/initializers/simple_form_custom.rb
@@ -1,4 +1,8 @@
 SimpleForm.setup do |config|
+  config.error_notification_class = 'alert alert-danger'
+  config.button_class = 'btn btn-primary'
+  config.boolean_style = :inline
+
   config.wrappers(
     :bootstrap3,
     tag: 'div',
@@ -11,10 +15,10 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label
+    b.use :label, class: 'control-label col-control-label'
 
     b.wrapper tag: 'div', class: 'col-control' do |ba|
-      ba.use :input
+      ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
@@ -32,24 +36,17 @@ SimpleForm.setup do |config|
     b.optional :pattern
     b.optional :min_max
     b.optional :readonly
-    b.use :label
+    b.use :label, class: 'control-label col-control-label'
 
     b.wrapper tag: 'div', class: 'col-control-wide' do |ba|
-      ba.use :input
+      ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
 
   config.default_wrapper = :bootstrap3
-
   config.wrapper_mappings = { text: :wide }
-
-  config.boolean_style = :inline
   config.browser_validations = true
-  config.button_class = 'btn btn-primary'
   config.form_class = 'form-horizontal'
-  config.input_class = 'form-control'
-  config.label_class = 'control-label col-control-label'
-  config.error_notification_class = 'alert alert-error'
 end
