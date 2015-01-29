c2ef855ecc1afb5cd0bbb172c4605deb155e82fd
Owen Davies <git@obduk.com>
2015-01-29 18:18:26 +0000

    Specify a default host for mailers

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -2,7 +2,7 @@ class AccountMailer < Devise::Mailer
   def unlock_instructions(record, token, opts = {})
     site = record.sites.first
 
-    @host = site.host
+    default_url_options[:host] = site.host
 
     super(record, token, opts.merge(from: site.email))
   end
