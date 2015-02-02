5724f69aeec9bbfdf8a06f989c8f967feba21aa3
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-02-02 13:39:03 +0000

    Added letter_opener gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,4 +29,6 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  config.action_mailer.delivery_method = :letter_opener_web
 end
