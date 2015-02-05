8ebda5713d3b3acb516f338a0fad73e1280e8f3c
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-02-05 22:28:13 +0000

    Added email template and premailer

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,14 +1,19 @@
 class MessageMailer < ActionMailer::Base
+  helper :application
+
+  layout 'mailer'
+
   def new_message(message)
     @message = message
 
-    site = message.site
-    from = Mail::Address.new(site.email)
-    from.display_name = site.name
+    @site = message.site
+
+    from = Mail::Address.new(@site.email)
+    from.display_name = @site.name
 
     mail(
       from: from,
-      to: site.users.map(&:email).sort,
+      to: @site.users.map(&:email).sort,
       subject: message.subject
     )
   end
