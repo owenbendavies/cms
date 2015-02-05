b570cbec397d0530075125f9c77dd2379eb41cd5
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-02-05 23:43:03 +0000

    Added email template to account_mailer

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,22 +1,26 @@
 class AccountMailer < Devise::Mailer
+  helper :application
+
+  layout 'mailer'
+
   def reset_password_instructions(record, token, opts = {})
-    site = record.sites.first
+    @site = record.sites.first
 
-    default_url_options[:host] = site.host
+    default_url_options[:host] = @site.host
 
-    from = Mail::Address.new(site.email)
-    from.display_name = site.name
+    from = Mail::Address.new(@site.email)
+    from.display_name = @site.name
 
     super(record, token, opts.merge(from: from))
   end
 
   def unlock_instructions(record, token, opts = {})
-    site = record.sites.first
+    @site = record.sites.first
 
-    default_url_options[:host] = site.host
+    default_url_options[:host] = @site.host
 
-    from = Mail::Address.new(site.email)
-    from.display_name = site.name
+    from = Mail::Address.new(@site.email)
+    from.display_name = @site.name
 
     super(record, token, opts.merge(from: from))
   end
