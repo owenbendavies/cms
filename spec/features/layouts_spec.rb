6f3f62fae9e735ec774765b3caee1c99d7b9d583
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-02-23 10:02:57 +0000

    Refactored layouts

--- spec/features/layouts_spec.rb
+++ spec/features/layouts_spec.rb
@@ -2,38 +2,34 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts', type: :feature do
-  shared_context 'layout' do |layout|
-    before do
-      site.main_menu_page_ids = [home_page.id, test_page.id]
-      site.main_menu_in_page = true
-      site.main_menu_in_footer = true
-      site.layout = layout
-      site.save!
+  it 'renders one_column layout' do
+    site.layout = 'one_column'
+    site.save!
 
-      visit_page '/home'
-    end
+    visit_page '/test_page'
 
-    it 'has all sections' do
-      expect(page).to have_selector 'header#page_header'
-      expect(page).to have_selector '#main_menu'
-      expect(page).to have_selector 'article#main_article'
-      expect(page).to have_selector '#footer_main_menu'
-      expect(page).to have_selector 'footer#page_footer'
-    end
+    expect(page).to have_selector '#main_article.col-sm-12 header'
   end
 
-  context 'one_column' do
-    it_behaves_like 'layout', 'one_column' do
-    end
+  it 'renders right_sidebar layout' do
+    site.layout = 'right_sidebar'
+    site.sidebar_html_content = '<h1>Test</h1>'
+    site.save!
+
+    visit_page '/test_page'
+
+    expect(page).to have_selector '#main_article.col-sm-8 header'
+    expect(page).to have_selector '#sidebar.col-sm-4 h1'
   end
 
-  %w(right_sidebar small_right_sidebar).each do |layout|
-    context layout do
-      it_behaves_like 'layout', layout do
-        it 'has sidebar' do
-          expect(page).to have_selector 'aside#sidebar'
-        end
-      end
-    end
+  it 'renders small_right_sidebar layout' do
+    site.layout = 'small_right_sidebar'
+    site.sidebar_html_content = '<h1>Test</h1>'
+    site.save!
+
+    visit_page '/test_page'
+
+    expect(page).to have_selector '#main_article.col-sm-9 header'
+    expect(page).to have_selector '#sidebar.col-sm-3 h1'
   end
 end
