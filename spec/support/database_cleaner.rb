3f3169816986a3d807595c910085ee8e6d0b757c
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-03-23 09:40:06 +0000

    Changed database cleaner to always truncation as just as quick

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,10 @@
 RSpec.configure do |config|
   config.before :all do
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before :each do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before :each, js: true do
     DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before :each do
     DatabaseCleaner.start
   end
 
-  config.after :each do
+  config.before :each do
     DatabaseCleaner.clean
   end
 end
