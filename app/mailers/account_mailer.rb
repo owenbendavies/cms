caddd4c9019760b1976b3382bca26a9f739d8bb4
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-06-04 12:21:55 +0000

    Removed unused line

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -7,8 +7,6 @@ class AccountMailer < Devise::Mailer
     super(record, token, opts.merge(from: from_site(@site)))
   end
 
-  alias :unlock_instructions :reset_password_instructions
-
   def unlock_instructions(record, token, opts = {})
     @site = record.sites.first
 
