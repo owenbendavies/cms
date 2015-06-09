143cb16cf985f8dd12165297c32db0fcbc1aca5a
Owen Davies <git@obduk.com>
2015-06-09 11:37:15 +0000

    Upgraded gems

--- spec/factories/page.rb
+++ spec/factories/page.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :page do
     site
     name { Faker::Name.name }
-    html_content { "<p>#{ Faker::Lorem.paragraph }</p>" }
+    html_content { "<p>#{Faker::Lorem.paragraph}</p>" }
     association :created_by, factory: :user
     association :updated_by, factory: :user
   end
--- spec/factories/site.rb
+++ spec/factories/site.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     google_analytics { "UA-#{Faker::Number.number(3)}-#{Faker::Number.digit}" }
     charity_number { Faker::Number.number(Faker::Number.digit.to_i) }
     stylesheet_filename { "#{Digest::MD5.hexdigest(rand.to_s)}.css" }
-    sidebar_html_content { "<h2>#{ Faker::Lorem.sentence }</h2>" }
+    sidebar_html_content { "<h2>#{Faker::Lorem.sentence}</h2>" }
 
     facebook { Faker::Internet.user_name(nil, ['.']) }
     twitter { Faker::Internet.user_name(nil, ['_'])[0, 15] }
