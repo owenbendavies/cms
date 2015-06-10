e0156c6d48ec2d6d9822cd9ae66709f7ce953f93
Owen Davies <git@obduk.com>
2015-06-10 11:20:56 +0000

    Added tests for gziped assets

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -116,14 +116,26 @@ RSpec.describe 'application', type: :feature do
   end
 
   it 'serves fonts up with CORS' do
-    font = Dir.glob(Rails.root.join('public/assets/**/*.woff')).first
+    font_path  = Dir.glob(Rails.root.join('public/assets/**/*.woff')).first
+    font_path.gsub!(Rails.root.join('public').to_s, '')
 
-    expect(font).to_not be_nil
+    expect(font_path).to_not be_blank
 
-    font.gsub!(Rails.root.join('public').to_s, '')
-
-    visit_page font
+    visit_page font_path
 
     expect(response_headers['Access-Control-Allow-Origin']).to eq '*'
+  end
+
+  it 'serves gziped assets' do
+    asset_file_path = Dir.glob(Rails.root.join('public/assets/*.gz')).first
+    asset_file_path.gsub!(Rails.root.join('public').to_s, '')
+    asset_file_path.gsub!(/\.gz$/, '')
+
+    expect(asset_file_path).to_not be_blank
+
+    page.driver.header('ACCEPT_ENCODING', 'gzip, deflate')
+    visit_page asset_file_path
+
+    expect(response_headers['Content-Encoding']).to eq 'gzip'
   end
 end
