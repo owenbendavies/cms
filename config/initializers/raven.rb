3d8b8f1d3e477de24feef07980671dca3a7edfb9
Owen Davies <git@obduk.com>
2015-06-24 11:00:01 +0000

    Show all errors in Sentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,9 +1,9 @@
-if defined? Raven
-  Raven.configure do |config|
-    if Rails.application.secrets.sentry_dsn
-      config.dsn = Rails.application.secrets.sentry_dsn
-    end
-
-    config.ssl_verification = true
+Raven.configure do |config|
+  if Rails.application.secrets.sentry_dsn
+    config.dsn = Rails.application.secrets.sentry_dsn
   end
+
+  config.excluded_exceptions = []
+
+  config.ssl_verification = true
 end
