32e8911d482de932ed331a67e0357aee247b3544
Owen Davies <git@obduk.com>
2015-06-25 08:50:11 +0000

    Refactored application_spec into unknown_routes_spec

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -6,37 +6,6 @@ RSpec.describe 'application', type: :feature do
     expect(current_path).to eq '/home'
   end
 
-  it 'renders page not found for urls with unkown format in' do
-    visit '/home.txt'
-    expect(page.status_code).to eq 404
-    expect(page).to have_content 'Page Not Found'
-  end
-
-  it 'renders page not found for unknown accept header' do
-    page.driver.header('Accept', 'application/json')
-    visit '/home'
-    expect(page.status_code).to eq 404
-    expect(page).to have_content 'Page Not Found'
-  end
-
-  it 'renders page not found for urls with capitals in' do
-    visit '/Home'
-    expect(page.status_code).to eq 404
-    expect(page).to have_content 'Page Not Found'
-  end
-
-  it 'renders page not found for urls with dots in path' do
-    visit '/file.pid/file'
-    expect(page.status_code).to eq 404
-    expect(page).to have_content 'Page Not Found'
-  end
-
-  it 'renders page not found for unknown url' do
-    visit '/badroute'
-    expect(page.status_code).to eq 404
-    expect(page).to have_content 'Page Not Found'
-  end
-
   context 'http' do
     before do
       visit_page '/home'
