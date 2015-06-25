009a890c9dd2824bcd8c9b0057369a0b250cb573
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-06-25 10:03:45 +0000

    Refactored loaderid_spec

--- spec/features/application/loaderid_spec.rb
+++ spec/features/application/loaderid_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe '/loaderio', type: :feature do
-  it 'renders loaderio verification' do
+RSpec.feature 'LoaderIO' do
+  scenario 'visit the page' do
     visit_page '/loaderio-12345.txt'
 
     expect(page).to have_content 'loaderio-12345'
     expect(response_headers['Content-Type']).to eq 'text/plain; charset=utf-8'
   end
 
-  it 'renders page not found when not txt' do
+  scenario 'with non txt extension' do
     visit '/loaderio-12345'
-    expect(page.status_code).to eq 404
+
     expect(page).to have_content 'Page Not Found'
+    expect(page.status_code).to eq 404
   end
 end
