4c2bc3dbabdc7cc243da4ea23fad3438d9060962
Owen Davies <git@obduk.com>
2015-06-25 08:51:11 +0000

    Refactored footer_spec into social_network_links_spec

--- spec/features/sites/edit_spec.rb
+++ spec/features/sites/edit_spec.rb
@@ -21,12 +21,6 @@ RSpec.feature 'Edit the site' do
       expect(find_field('Charity number').value).to eq site.charity_number
       expect(find_field('Main menu')).to_not be_checked
 
-      expect(find_field('Facebook').value).to eq site.facebook
-      expect(find_field('Twitter').value).to eq site.twitter
-      expect(find_field('YouTube').value).to eq site.youtube
-      expect(find_field('LinkedIn').value).to eq site.linkedin
-      expect(find_field('GitHub').value).to eq site.github
-
       fill_in 'Name', with: "  #{new_company_name} "
       fill_in 'Sub title', with: "  #{new_catch_phrase} "
       select 'Right sidebar', from: 'Layout'
@@ -35,12 +29,6 @@ RSpec.feature 'Edit the site' do
       fill_in 'Copyright', with: " #{new_name} "
       fill_in 'Charity number', with: " #{new_number} "
       check 'Main menu'
-
-      fill_in 'Facebook', with: " #{new_facebook} "
-      fill_in 'Twitter', with: " #{new_twitter} "
-      fill_in 'YouTube', with: " #{new_youtube} "
-      fill_in 'LinkedIn', with: "  #{new_linkedin} "
-      fill_in 'GitHub', with: "  #{new_github} "
 
       click_button 'Update Site'
 
@@ -57,35 +45,23 @@ RSpec.feature 'Edit the site' do
       expect(site.copyright).to eq new_name
       expect(site.charity_number).to eq new_number.to_s
       expect(site.main_menu_in_footer).to eq true
-
-      expect(site.facebook).to eq new_facebook
-      expect(site.twitter).to eq new_twitter
-      expect(site.youtube).to eq new_youtube
-      expect(site.linkedin).to eq new_linkedin
-      expect(site.github).to eq new_github
     end
 
     scenario 'adding Google Analytics' do
       expect(body).to_not include "ga('create',"
-      expect(find_field('Google Analytics').value).to eq site.google_analytics
 
       new_code = "UA-#{Faker::Number.number(3)}-#{Faker::Number.digit}"
 
       fill_in 'Google Analytics', with: "  #{new_code} "
       click_button 'Update Site'
 
-      expect(current_path).to eq '/home'
       expect(page).to have_content 'Site successfully updated'
+      expect(body).to include "ga('create', '#{new_code}', 'auto');"
+      expect(body).to include "ga('set', '&uid', '#{user.id}');"
 
-      site.reload
+      visit_page go_to_url
 
-      expect(site.google_analytics).to eq new_code
-
-      expect(body).to include(
-        "ga('create', '#{site.google_analytics}', 'auto');"
-      )
-
-      expect(body).to include("ga('set', '&uid', '#{user.id}');")
+      expect(find_field('Google Analytics').value).to eq new_code
     end
 
     scenario 'with empty copyright' do
