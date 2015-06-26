695a53ad6bfb552a48494c522117ead9314abfca
Owen Davies <git@obduk.com>
2015-06-26 09:31:08 +0000

    Refactored application_spec tests into web_server_spec

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -66,28 +66,4 @@ RSpec.describe 'application', type: :feature do
     expect(response_headers['Set-Cookie'])
       .to match(/^_cms_session=[0-9a-f]{32};.*/)
   end
-
-  it 'serves fonts up with CORS' do
-    font_path  = Dir.glob(Rails.root.join('public/assets/**/*.woff')).first
-    font_path.gsub!(Rails.root.join('public').to_s, '')
-
-    expect(font_path).to_not be_blank
-
-    visit_page font_path
-
-    expect(response_headers['Access-Control-Allow-Origin']).to eq '*'
-  end
-
-  it 'serves gziped assets' do
-    asset_file_path = Dir.glob(Rails.root.join('public/assets/*.gz')).first
-    asset_file_path.gsub!(Rails.root.join('public').to_s, '')
-    asset_file_path.gsub!(/\.gz$/, '')
-
-    expect(asset_file_path).to_not be_blank
-
-    page.driver.header('ACCEPT_ENCODING', 'gzip, deflate')
-    visit_page asset_file_path
-
-    expect(response_headers['Content-Encoding']).to eq 'gzip'
-  end
 end
