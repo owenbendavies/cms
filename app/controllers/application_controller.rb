b7fdacb74160e4ac53b76609bd50dd15cd07938d
Owen Davies <git@obduk.com>
2015-06-30 13:50:46 +0000

    Don't raise exception for bad CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery
 
   rescue_from ActionView::MissingTemplate, with: :page_not_found
   rescue_from ActionController::UnknownFormat, with: :page_not_found
--- spec/features/application/web_server_spec.rb
+++ spec/features/application/web_server_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature 'Web server' do
+  before do
+    ActionController::Base.allow_forgery_protection = true
+  end
+
+  after do
+    ActionController::Base.allow_forgery_protection = false
+  end
+
   scenario 'visiting a font' do
     font_path = Dir.glob(Rails.root.join('public/assets/**/*.woff')).first
     font_path.gsub!(Rails.root.join('public').to_s, '')
@@ -32,5 +40,22 @@ RSpec.feature 'Web server' do
     visit '/home'
 
     expect(page.status_code).to eq 403
+  end
+
+  scenario 'not sending CSRF token' do
+    visit '/login'
+
+    find('input[name=authenticity_token]', visible: false).set 'bad'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Login'
+
+    expect(page.status_code).to eq 200
+    expect(page.current_path).to eq '/home'
+
+    visit '/users/edit'
+
+    expect(page.current_path).to eq '/login'
   end
 end
