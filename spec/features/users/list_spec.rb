0b6087d925c30a31bb9a1c352867a34796a29667
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-07-14 11:26:59 +0000

    Refactored factories to improve speed

--- spec/features/users/list_spec.rb
+++ spec/features/users/list_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.feature 'List users' do
   let(:go_to_url) { '/site/users' }
 
+  let(:user2) { FactoryGirl.create(:user) }
+
+  before do
+    site.users << user2
+  end
+
   it_behaves_like 'restricted page'
 
   it_behaves_like 'logged in user' do
@@ -11,7 +17,7 @@ RSpec.feature 'List users' do
         expect(page).to have_content 'Users'
         expect(page).to have_content 'Email'
         expect(page).to have_content user.email
-        expect(page).to have_content site.users.second.email
+        expect(page).to have_content user2.email
       end
     end
 
