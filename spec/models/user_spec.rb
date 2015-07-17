01e943b321d0474b693104381d0eb3bd33548469
Owen Davies <git@obduk.com>
2015-07-17 18:04:06 +0000

    Validate name is at least 3 characters long

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -84,6 +84,8 @@ RSpec.describe User do
     it { should_not allow_value('password').for(:password) }
 
     it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_least(3) }
   end
 
   describe '#all_sites' do
