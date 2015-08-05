8262fc618bd4a193db01224c1e4165f4f5b40c90
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-08-05 13:38:57 +0000

    Removed uneeded module in mailer_helpers

--- spec/support/mailer_helpers.rb
+++ spec/support/mailer_helpers.rb
@@ -1,27 +1,23 @@
-module MailerHelpers
-  RSpec.shared_context 'site email' do
-    before do
-      RequestStore.store[:site] = site
-    end
+RSpec.shared_context 'site email' do
+  before do
+    RequestStore.store[:site] = site
+  end
 
-    it 'has from address as site email' do
-      expect(subject.from).to eq [site.email]
-    end
+  it 'has from address as site email' do
+    expect(subject.from).to eq [site.email]
+  end
 
-    it 'has from name as site name' do
-      addresses = subject.header['from'].address_list.addresses
-      expect(addresses.first.display_name).to eq site.name
-    end
+  it 'has from name as site name' do
+    addresses = subject.header['from'].address_list.addresses
+    expect(addresses.first.display_name).to eq site.name
+  end
 
-    it 'has site name in body' do
-      expect(subject.body).to have_content site.name
-    end
+  it 'has site name in body' do
+    expect(subject.body).to have_content site.name
+  end
 
-    it 'has copyright in body' do
-      body = subject.body
-      expect(body).to have_content "#{site.copyright} © #{Time.zone.now.year}"
-    end
+  it 'has copyright in body' do
+    body = subject.body
+    expect(body).to have_content "#{site.copyright} © #{Time.zone.now.year}"
   end
 end
-
-RSpec.configuration.include MailerHelpers
