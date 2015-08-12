109160c8fb99c7bf5f8069c092ec1a7c2dfaa824
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-08-12 16:21:01 +0000

    Enforce site settings are unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717135232) do
+ActiveRecord::Schema.define(version: 20150812150017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20150717135232) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+  add_index "site_settings", ["user_id", "site_id"], name: "index_site_settings_on_user_id_and_site_id", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",  null: false, index: {name: "index_versions_on_item_type_and_item_id", with: ["item_id"], using: :btree}
