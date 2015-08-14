3e51f90072cddd13483a6d9703b949c8fc927c0a
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-08-14 11:05:10 +0000

    Added sitemap to systems_controller

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,5 +1,5 @@
 class SystemsController < ApplicationController
-  PUBLIC_PAGES = [:health, :home, :loader_io, :robots, :timeout]
+  PUBLIC_PAGES = [:health, :home, :loader_io, :robots, :sitemap, :timeout]
 
   skip_before_action :render_site_not_found, only: [:health]
   skip_before_action :authenticate_user!, only: PUBLIC_PAGES
@@ -24,8 +24,28 @@ class SystemsController < ApplicationController
   def robots
   end
 
+  def sitemap
+    @pages = @site.pages
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: xml_sitemap.render }
+    end
+  end
+
   def timeout
     sleep params[:seconds].to_f
     render text: 'ok'
+  end
+
+  private
+
+  def xml_sitemap
+    XmlSitemap::Map.new(@site.host, home: false, secure: request.ssl?) do |map|
+      @pages.each do |page|
+        next if page.private?
+        map.add page_path(page.to_param), updated: page.updated_at
+      end
+    end
   end
 end
---
+++ spec/features/system/sitemap_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.feature 'Sitemap' do
+  let!(:private_page) { FactoryGirl.create(:private_page) }
+
+  context 'html' do
+    let(:go_to_url) { '/sitemap' }
+
+    scenario 'visiting the page' do
+      visit_page go_to_url
+
+      expect(page).to have_link 'Home', href: '/home'
+      expect(page).to have_no_link private_page.name
+    end
+
+    scenario 'navigating to the page via footer' do
+      visit_page '/home'
+
+      within('#cms-footer-links') do
+        click_link 'Sitemap'
+      end
+
+      expect(current_path).to eq go_to_url
+    end
+
+    it_behaves_like 'logged in user' do
+      scenario 'with a private page' do
+        expect(page).to have_no_link private_page.name
+      end
+    end
+
+    it_behaves_like 'logged in site user' do
+      scenario 'with a private page' do
+        find('ul#cms-sitemap li:nth-child(2)').tap do |item|
+          expect(item).to have_link private_page.name, href: '/private'
+          expect(item).to have_selector '.fa-lock'
+        end
+      end
+
+      include_examples 'page with topbar link', 'Sitemap', 'sitemap'
+    end
+  end
+
+  context 'xml' do
+    let(:go_to_url) { '/sitemap.xml' }
+
+    scenario 'visiting the page' do
+      visit_page go_to_url
+
+      home_path = 'http://localhost/home'
+      expect(find(:xpath, '//urlset/url[1]/loc').text).to eq home_path
+
+      updated_at = test_page.updated_at.iso8601
+
+      expect(find(:xpath, '//urlset/url[1]/lastmod').text).to eq updated_at
+
+      expect(page).to have_no_xpath('//loc', text: "http://localhost/#{private_page.url}")
+    end
+
+    scenario 'visiting with https' do
+      page.driver.header('X-Forwarded-Proto', 'https')
+      visit_page go_to_url
+
+      home_path = 'https://localhost/home'
+      expect(find(:xpath, '//urlset/url[1]/loc').text).to eq home_path
+    end
+  end
+end
