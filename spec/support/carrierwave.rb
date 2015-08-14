23071e9ae60a5777d758aa73d3174802ad394e85
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-08-14 09:10:00 +0000

    Allow line lengths to be 100 characters to make code neater

--- spec/support/carrierwave.rb
+++ spec/support/carrierwave.rb
@@ -1,16 +1,10 @@
 RSpec.configuration.before :each do
-  FileUtils.rm_rf File.join(
-    CarrierWave.root,
-    Rails.application.secrets.uploads_store_dir
-  )
+  FileUtils.rm_rf File.join(CarrierWave.root, Rails.application.secrets.uploads_store_dir)
 end
 
 module CarrierWaveHelpers
   def uploaded_files
-    uploads_dir = File.join(
-      CarrierWave.root,
-      Rails.application.secrets.uploads_store_dir
-    )
+    uploads_dir = File.join(CarrierWave.root, Rails.application.secrets.uploads_store_dir)
 
     files = File.join(uploads_dir, '**', '*')
 
