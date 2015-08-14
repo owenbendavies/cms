5d22a6e0298f8a69017264e750b90efb80c65d67
Owen Davies <git@obduk.com>
2015-08-14 10:20:55 +0000

    Create systems controller

--- app/controllers/test_routes_controller.rb
+++
@@ -1,9 +0,0 @@
-class TestRoutesController < ApplicationController
-  skip_before_action :authenticate_user!
-  skip_authorization_check
-
-  def timeout
-    sleep params[:seconds].to_f
-    render text: 'ok'
-  end
-end
