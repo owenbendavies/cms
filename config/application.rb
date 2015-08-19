541a096c6b2c84bd27866047a9cb09792f5be02c
Owen Davies <git@obduk.com>
2015-08-19 16:48:29 +0000

    Updated config to match default rails config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,14 +17,26 @@ Bundler.require(*Rails.groups)
 
 module Cms
   class Application < Rails::Application
-    locals = Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
-    config.i18n.load_path += Dir[locals]
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-    config.i18n.enforce_available_locales = true
-    config.action_view.raise_on_missing_translations = true
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # Raises error for missing translations
+    config.action_view.raise_on_missing_translations = true
+    config.i18n.enforce_available_locales = true
+
+    # Lograge options
     config.lograge.custom_options = lambda do |event|
       {
         host: event.payload[:host],
@@ -26,6 +47,7 @@ module Cms
       }
     end
 
+    # Customer middleware
     config.middleware.use Rack::Protection
   end
 end
