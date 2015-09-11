71d29c6b3bc1b15236ade9363ffabe181cd5fa5a
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-09-11 14:10:06 +0000

    Changed Redis config to work with Herkou

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.application.secrets.redis_server
-  Rails.application.config.session_store(
-    :redis_store,
-    expire_after: 30.days,
-    key: '_cms_session',
-    redis_server: Rails.application.secrets.redis_server + ':session'
-  )
-end
+Rails.application.config.session_store(
+  :redis_store,
+  expire_after: 30.days,
+  key: '_cms_session',
+  redis_server: Rails.application.secrets.redis_url
+)
