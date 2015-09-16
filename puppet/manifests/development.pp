2efd8b90e223891dea7691dceff30dc6686197e8
Owen Davies <git@obduk.com>
2015-09-16 13:21:45 +0000

    Made vagrant cd to correct dir

--- puppet/manifests/development.pp
+++ puppet/manifests/development.pp
@@ -1,1 +1,16 @@
 include cms::development
+include docker
+
+apt::source { 'heroku':
+  include_src => false,
+  key         => '0F1B0520',
+  key_source  => 'https://toolbelt.heroku.com/apt/release.key',
+  location    => 'http://toolbelt.heroku.com/ubuntu',
+  release     => '',
+  repos       => './',
+} ->
+package { 'heroku-toolbelt': ensure => present }
+
+file { '/etc/profile.d/vagrant.sh':
+  content => 'if [ -n "$BASH_VERSION" ]; then cd /vagrant; fi'
+}
