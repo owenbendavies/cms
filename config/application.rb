c17adea1266d53eada319be86f64b68741d14f92
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2015-09-18 11:47:28 +0000

    Remove active_job as not used

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
-require 'active_job/railtie'
+# require 'active_job/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
