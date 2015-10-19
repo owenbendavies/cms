fac5c4a5a4aa9670f0823bcb6693c115dbecbefa
Owen Davies <git@obduk.com>
2015-10-19 15:09:54 +0100

    Removed Heroku from puppet as now run on Mac

--- puppet/manifests/development.pp
+++ puppet/manifests/development.pp
@@ -1,15 +1,5 @@
 include cms::development
 
-apt::source { 'heroku':
-  include_src => false,
-  key         => '0F1B0520',
-  key_source  => 'https://toolbelt.heroku.com/apt/release.key',
-  location    => 'http://toolbelt.heroku.com/ubuntu',
-  release     => '',
-  repos       => './',
-} ->
-package { 'heroku-toolbelt': ensure => present }
-
 file { '/etc/profile.d/vagrant.sh':
   content => 'if [ -n "$BASH_VERSION" ]; then cd /vagrant; fi'
 }
