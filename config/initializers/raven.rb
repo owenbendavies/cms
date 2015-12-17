9e4e8a4f0be70e846a4cf7f4fadd7660b9ab8678
Owen Davies <git@obduk.com>
2015-12-17 14:19:51 +0000

    Don't report routing errors in raven

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,7 +2,9 @@ if defined?(Raven) && Rails.application.secrets.sentry_dsn
   Raven.configure do |config|
     config.dsn = Rails.application.secrets.sentry_dsn
 
-    config.excluded_exceptions = []
+    config.excluded_exceptions = [
+      'ActionController::RoutingError'
+    ]
 
     config.ssl_verification = true
 
