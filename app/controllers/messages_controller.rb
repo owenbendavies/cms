c78b916613b2fb7b2e99a2882798061196ce8eac
Owen Davies <git@obduk.com>
2016-01-05 12:18:22 +0000

    Upgraded gems

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  site_id    :integer          not null
+#  subject    :string(64)       not null
+#  name       :string(64)       not null
+#  email      :string(64)       not null
+#  phone      :string(32)
+#  message    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_agent :text
+#  ip_address :string(45)
+#
+# Indexes
+#
+#  fk__messages_site_id  (site_id)
+#
+# Foreign Keys
+#
+#  fk_messages_site_id  (site_id => sites.id)
+#
+
 class MessagesController < ApplicationController
   authorize_resource :site
   load_and_authorize_resource through: :site
