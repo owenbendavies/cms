0ba28a5d2d4bc1b62865ce9334a73e8b83f3e34b
Owen Davies <obduk@users.noreply.github.com>
2016-01-09 12:02:09 +0000

    Removed Docker as now just use Heroku

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,30 @@
+package {
+  [
+    'imagemagick',
+    'libqt5webkit5-dev',
+    'qt5-default',
+    'redis-server',
+    'xvfb',
+  ]:
+}
+
+rbenv::install { 'vagrant': }
+rbenv::compile { '2.2.4': user => 'vagrant' }
+
+include postgresql::client
+include postgresql::server
+
+class { 'postgresql::lib::devel': link_pg_config => false }
+
+postgresql::server::pg_hba_rule { 'trust localhost TCP access to all users':
+  type        => 'host',
+  database    => 'all',
+  user        => 'all',
+  address     => '127.0.0.1/32',
+  auth_method => 'trust',
+  order       => '002',
+}
+
+file { '/etc/profile.d/vagrant.sh':
+  content => 'if [ -n "$BASH_VERSION" ]; then cd /vagrant; fi'
+}
--- puppet/manifests/development.pp
+++
@@ -1,5 +0,0 @@
-include cms::development
-
-file { '/etc/profile.d/vagrant.sh':
-  content => 'if [ -n "$BASH_VERSION" ]; then cd /vagrant; fi'
-}
