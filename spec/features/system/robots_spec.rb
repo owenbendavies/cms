736a9a1dbafe3ef4e28b201bd61587f6693b38ab
Owen Davies <obduk@users.noreply.github.com>
2016-01-13 15:28:21 +0000

    Enabled force_ssl

--- spec/features/system/robots_spec.rb
+++ spec/features/system/robots_spec.rb
@@ -2,23 +2,17 @@ require 'rails_helper'
 
 RSpec.feature 'robots.txt' do
   scenario 'visiting the page' do
+    page.driver.header('X-Forwarded-Proto', 'https')
     visit_200_page '/robots.txt'
 
     expect(body).to eq <<EOF
-Sitemap: http://localhost:37511/sitemap.xml
+Sitemap: https://localhost:37511/sitemap.xml
 
 User-agent: *
 Disallow:
 EOF
 
     expect(response_headers['Content-Type']).to eq 'text/plain; charset=utf-8'
-  end
-
-  scenario 'visiting via https' do
-    page.driver.header('X-Forwarded-Proto', 'https')
-    visit_200_page '/robots.txt'
-
-    expect(body).to include 'https://localhost:37511/sitemap.xml'
   end
 
   scenario 'with non txt extension' do
--- spec/features/users/sites_spec.rb
+++ spec/features/users/sites_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 RSpec.feature 'User sites' do
   let(:go_to_url) { '/user/sites' }
+  let(:link_title) { 'localhost' }
+  let(:link_href) { 'https://localhost' }
 
   include_examples 'authenticated page'
 
   as_a 'logged in user' do
     scenario 'visiting the page' do
-      expect(page).to_not have_link 'localhost', href: 'http://localhost'
+      expect(page).to_not have_link link_title, href: link_href
     end
   end
 
   as_a 'logged in site user' do
     scenario 'visiting the page' do
-      expect(page).to have_link 'localhost', href: 'http://localhost'
+      expect(page).to have_link link_title, href: link_href
     end
 
     include_examples 'page with topbar link', 'Sites', 'list'
