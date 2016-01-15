1cc0d4db3f626f3d19f454b71e0856a84810a5f7
Owen Davies <obduk@users.noreply.github.com>
2016-01-15 13:54:51 +0000

    Removed hostname from action_mailer config

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -3,6 +3,5 @@ ActionMailer::Base.smtp_settings = {
   port: Rails.application.secrets.smtp_port,
   authentication: :plain,
   user_name: Rails.application.secrets.smtp_username,
-  password: Rails.application.secrets.smtp_password,
-  domain: `hostname -f`.strip
+  password: Rails.application.secrets.smtp_password
 }
