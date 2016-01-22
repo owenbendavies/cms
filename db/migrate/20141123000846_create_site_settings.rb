e8ebee3dbb262c00f28fda255f0fed0fd6d39836
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2016-01-22 17:16:23 +0000

    Fixed site_settings db column order

--- db/migrate/20141123000846_create_site_settings.rb
+++ db/migrate/20141123000846_create_site_settings.rb
@@ -1,12 +1,8 @@
 class CreateSiteSettings < ActiveRecord::Migration
   def change
-    create_table :site_settings, id: false do |table|
+    create_table :site_settings do |table|
       table.references :user, null: false
       table.references :site, null: false
-    end
-
-    change_table :site_settings do |table|
-      table.column :id, :primary_key
 
       table.timestamps null: false
     end
