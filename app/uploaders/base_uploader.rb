f2b6a7dc3e1f8e0b1a7e5e4314340b013acb085f
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2016-04-19 12:04:38 +0100

    Change s3 locations

---
+++ app/uploaders/base_uploader.rb
@@ -0,0 +1,19 @@
+class BaseUploader < CarrierWave::Uploader::Base
+  def uuid
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
+  def filename
+    return unless original_filename
+    return model.read_attribute(mounted_as) if model.read_attribute(mounted_as)
+    extension = file.extension.downcase.gsub('jpeg', 'jpg')
+    "#{uuid}.#{extension}"
+  end
+
+  def full_filename(for_file)
+    ext = File.extname(for_file)
+    base_name = for_file.chomp(ext)
+    [base_name, version_name || 'original'].join('/') + ext
+  end
+end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,28 +1,12 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < BaseUploader
   include CarrierWave::MiniMagick
-
-  delegate :store_dir, to: :model
 
   def extension_white_list
     %w(jpg jpeg png)
   end
 
-  def uuid
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
-  end
-
-  def filename
-    return unless original_filename
-    return model.read_attribute(mounted_as) if model.read_attribute(mounted_as)
-    extension = file.extension.gsub('jpeg', 'jpg').downcase
-    "#{uuid}.#{extension}"
-  end
-
-  def full_filename(for_file)
-    ext = File.extname(for_file)
-    base_name = for_file.chomp(ext)
-    [base_name, version_name].compact.join('_') + ext
+  def store_dir
+    'images'
   end
 
   version :span12 do
