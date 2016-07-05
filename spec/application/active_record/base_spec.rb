e13613925facdc1c25fc5642d41ae98b3c2e885d
Owen Davies <obduk@users.noreply.github.com>
2016-07-05 16:50:15 +0100

    Add a database timeout

---
+++ spec/application/active_record/base_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ActiveRecord::Base do
+  describe 'timeout' do
+    it 'allows fast queries' do
+      described_class.connection.execute('select pg_sleep(0.5);')
+    end
+
+    it 'fails on slow queries' do
+      expect do
+        described_class.connection.execute('select pg_sleep(2);')
+      end.to raise_error(ActiveRecord::StatementInvalid, /statement timeout/)
+    end
+  end
+end
