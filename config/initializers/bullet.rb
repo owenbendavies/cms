3f08c9c96ec0b1df8e0016a6cc68d8017f4f4778
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2016-09-25 21:19:49 +0100

    Enable Bullet in development

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,12 @@
+if defined? Bullet
+  Rails.application.config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+
+    Bullet.add_whitelist(
+      type: :n_plus_one_query,
+      class_name: 'Site',
+      association: :main_menu_pages
+    )
+  end
+end
