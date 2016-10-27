dc52a0e508f2e96e7c2017bd873e09931d92c67c
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2016-10-27 19:20:49 +0100

    Notify Job errors via Rollbar
    Change Job errors from Email to Rollbar so easier to track and manage.
    
    Resolves #614

--- app/jobs/validate_data_job.rb
+++ app/jobs/validate_data_job.rb
@@ -2,9 +2,11 @@ class ValidateDataJob < BaseJob
   NON_MODEL_TABLES = %w(delayed_jobs schema_migrations versions).freeze
 
   def perform
-    errors = model_errors
-
-    error(I18n.t('jobs.validate_data_job.error'), errors) if errors.any?
+    models.each do |model|
+      model.find_each.reject(&:valid?).each do |record|
+        error("#{model}##{record.id}: " + record.errors.full_messages.join(', '))
+      end
+    end
   end
 
   private
@@ -13,13 +15,5 @@ class ValidateDataJob < BaseJob
     all_table_names = ActiveRecord::Base.connection.tables
     model_tables = all_table_names - NON_MODEL_TABLES
     model_tables.sort.map(&:classify).map(&:constantize)
-  end
-
-  def model_errors
-    models.map do |model|
-      model.find_each.reject(&:valid?).map do |record|
-        "#{model}##{record.id}: " + record.errors.full_messages.join(', ')
-      end
-    end.flatten
   end
 end
