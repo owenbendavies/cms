477803beead0f7c8fb5b698c572c722eb638eb33
Owen Davies <obduk@users.noreply.github.com>
2016-11-12 11:21:06 +0000

    Remove CoffeeScript
    Remove CoffeeScript as going out of fashion.
    
    Resolves #644

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery-ujs
+//= require autosize
+//= require rails-timeago
+//= require turbolinks
+//= require google-analytics-turbolinks
+//= require bootstrap-custom
+//= require main
