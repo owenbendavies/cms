ce419bbb69b1794df9ab36ba6eb6a14e65a2e4de
Owen Davies <obduk@users.noreply.github.com>
2017-01-04 19:38:16 +0000

    Remove old annotations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,3 @@
-# == Schema Information
-#
-# Table name: messages
-#
-#  id         :integer          not null, primary key
-#  site_id    :integer          not null
-#  subject    :string(64)       not null
-#  name       :string(64)       not null
-#  email      :string(64)       not null
-#  phone      :string(32)
-#  message    :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  fk__messages_site_id  (site_id)
-#
-# Foreign Keys
-#
-#  fk_messages_site_id  (site_id => sites.id)
-#
-
 class MessagesController < ApplicationController
   def index
     authorize Message
