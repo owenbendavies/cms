8196f2d589990c36729b92ccc122b58485fef717
Owen Davies <obduk@users.noreply.github.com>
2017-01-14 11:03:27 +0000

    Added footer links

---
+++ spec/features/sites/footer_links_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature 'Footer links' do
+  let(:css_selector) { 'footer #cms-site-footer-links' }
+  let(:go_to_url) { '/home' }
+
+  scenario 'site with footer links' do
+    link1 = FactoryGirl.create(:footer_link, site: site)
+    FactoryGirl.create(:footer_link, site: site, icon: 'facebook-official')
+
+    visit_200_page
+
+    expect(page).to have_selector css_selector
+    expect(page).to have_link link1.name, href: link1.url
+
+    within css_selector do
+      expect(page).to have_selector '.fa-facebook-official'
+    end
+  end
+
+  scenario 'site without footer links' do
+    visit_200_page
+
+    expect(page).not_to have_selector css_selector
+  end
+end
