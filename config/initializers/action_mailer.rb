0d23f1a27bba5cc4deb5579c8bb9c3e971d0ce7c
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2017-01-22 11:41:50 +0000

    Change AWS credentials
    Resolves #705

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,20 +2,20 @@ if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD']
+    user_name: ENV.fetch('SENDGRID_USERNAME'),
+    password: ENV.fetch('SENDGRID_PASSWORD')
   }
-elsif ENV['AWS_REGION'] && ENV['AWS_IAM_KEY'] && ENV['AWS_IAM_SECRET']
+elsif ENV['AWS_REGION'] && ENV['AWS_ACCESS_KEY_ID'] && ENV['AWS_SECRET_ACCESS_KEY']
   signature = OpenSSL::HMAC.digest(
     OpenSSL::Digest.new('sha256'),
-    ENV['AWS_IAM_SECRET'],
+    ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     'SendRawEmail'
   )
 
   ActionMailer::Base.smtp_settings = {
-    address: "email-smtp.#{ENV['AWS_REGION']}.amazonaws.com",
+    address: "email-smtp.#{ENV.fetch('AWS_REGION')}.amazonaws.com",
     port: 587,
-    user_name: ENV['AWS_IAM_KEY'],
+    user_name: ENV.fetch('AWS_ACCESS_KEY_ID'),
     password: Base64.encode64(2.chr + signature).strip
   }
 end
