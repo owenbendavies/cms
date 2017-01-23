83bd11b3fe1276ebac12af62c19697234c80c657
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2017-01-23 09:03:42 +0000

    Change development web timeout and workers

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,2 +1,4 @@
-Rack::Timeout.timeout = Integer(ENV['WEB_TIMEOUT'] || 2)
+require 'rack/timeout/rollbar'
+
+Rack::Timeout.service_timeout = Integer(ENV['WEB_TIMEOUT'] || 2)
 Rack::Timeout::Logger.disable
