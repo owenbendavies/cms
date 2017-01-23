c4fae691a70b7fa295a448ad3e4c4e3b18a2eefc
Owen Davies <obduk@users.noreply.github.com>
2017-01-23 18:54:08 +0000

    Hide sequential Ids
    Resolves #709

--- spec/factories/message_factory.rb
+++ spec/factories/message_factory.rb
@@ -11,6 +11,7 @@
 #  message    :text             not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  uuid       :string           not null
 #
 # Indexes
 #
