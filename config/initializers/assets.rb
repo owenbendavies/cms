2a70c2cde0cb00364676cd141c525714c10a5bef
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2017-03-16 06:57:11 +0000

    Upgrade TinyMCE

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,8 @@ Rails.application.config.assets.version = '5.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.tinymce.install = :copy
+
 Rails.application.config.public_file_server.enabled = true
 
 if ENV['DEV_ASSETS']
