e167e6a2870d977bf9cec9bce37692f81e4f7499
Owen Davies <obduk@users.noreply.github.com>
2017-03-25 07:56:27 +0000

    Fix Rollbar error #205 - JS errors
    Resolves #759

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,9 @@ Rails.application.config.tinymce.install = :copy
 Rails.application.config.public_file_server.enabled = true
 
 Rails.application.config.public_file_server.headers = {
+  'Access-Control-Allow-Methods' => 'get',
+  'Access-Control-Allow-Origin' => '*',
+  'Access-Control-Max-Age' => '600',
   'Cache-Control' => "public, max-age=#{1.year.to_i}"
 }
 
