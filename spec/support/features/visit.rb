9a3e73a4868979abae536c6e94b8c1e67e53ecf3
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2017-04-21 20:34:40 +0100

    Change to official way of detecting Javascript errors

--- spec/support/features/visit.rb
+++ spec/support/features/visit.rb
@@ -25,7 +25,6 @@ RSpec.shared_context 'visit' do
   def visit_non_redirect(url = go_to_url)
     unchecked_visit url
     expect(current_path).to eq URI.parse(url).path
-    check_no_js_errors
   end
 
   def visit_200_page(url = go_to_url)
@@ -43,10 +42,6 @@ RSpec.shared_context 'visit' do
     visit_non_redirect url
     expect(page.status_code).to eq 406
     expect(page.body).to be_empty
-  end
-
-  def check_no_js_errors
-    expect(page.driver.error_messages).to be_empty if page.driver.class == Capybara::Webkit::Driver
   end
 end
 
