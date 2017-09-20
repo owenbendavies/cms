e01f41c589f6cdfca9a0e184dd1f5331083d4dc3
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2017-09-20 20:12:55 +0100

    Use Rollbar Gem js
    Resolves #869

--- spec/requests/application/rollbar_spec.rb
+++ spec/requests/application/rollbar_spec.rb
@@ -4,11 +4,17 @@ RSpec.describe 'Application rollbar' do
   let(:request_method) { :get }
   let(:request_path) { '/sitemap' }
   let(:rollbar_config) { 'rollbarConfig' }
+  let(:rollbar_js) { 'rollbarJsUrl' }
 
   context 'without a token' do
-    it 'does not contain Rollbar js' do
+    it 'does not contain Rollbar config' do
       request_page
       expect(body).not_to include rollbar_config
+    end
+
+    it 'does not contain Rollbar js' do
+      request_page
+      expect(body).not_to include rollbar_js
     end
   end
 
@@ -16,9 +22,14 @@ RSpec.describe 'Application rollbar' do
     let(:rollbar_client_token) { 'xxxxx' }
     let(:environment_variables) { { ROLLBAR_CLIENT_TOKEN: rollbar_client_token } }
 
-    it 'contains Rollbar js' do
+    it 'contains Rollbar config' do
       request_page
       expect(body).to include rollbar_config
+    end
+
+    it 'contains Rollbar js' do
+      request_page
+      expect(body).to include rollbar_js
     end
 
     it 'contains Rollbar token' do
