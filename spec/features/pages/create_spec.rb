3913e551c33fa48438713c6b0ba085600669a341
Owen Davies <obduk@users.noreply.github.com>
2017-12-22 21:07:59 +0000

    Fix randomly failing tests

--- spec/features/pages/create_spec.rb
+++ spec/features/pages/create_spec.rb
@@ -4,19 +4,17 @@ RSpec.feature 'Creating a page' do
   before do
     login_as site_user
     navigate_via_topbar menu: 'Page', title: 'New Page', icon: 'plus'
+    find('.mce-content-body')
   end
 
   scenario 'valid data' do
     fill_in 'Name', with: new_name
 
-    find('.mce-content-body')
     find('.js-tinymce').click
     find('.js-tinymce').base.send_keys(new_message)
 
-    expect do
-      click_button 'Create Page'
-      expect(page).to have_content new_name
-    end.to change(Page, :count).by(1)
+    click_button 'Create Page'
+    expect(page).to have_content new_name
 
     new_page = Page.find_by!(site_id: site.id, name: new_name)
     expect(new_page.html_content).to eq "<p>#{new_message}</p>"
