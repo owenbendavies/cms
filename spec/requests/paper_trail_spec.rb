3578a553bc1b99d443a6ff6838fca13f71355e23
Owen Davies <git@obduk.com>
2018-02-16 22:32:41 +0000

    Record papertrail user in API

--- spec/requests/paper_trail_spec.rb
+++ spec/requests/paper_trail_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'paper trail' do
-  context 'when updating something' do
+  context 'when updating something via the frontend' do
     let(:request_user) { FactoryBot.create(:user, site: site) }
     let(:request_method) { :put }
     let(:request_path) { '/site' }
@@ -13,6 +13,29 @@ RSpec.describe 'paper trail' do
 
       site = Site.find_by!(name: new_name)
       expect(site.versions.last.whodunnit).to eq request_user.id.to_s
+    end
+  end
+
+  context 'when creating something via the api' do
+    let(:request_method) { :post }
+    let(:request_path) { '/api/messages' }
+    let(:request_user) { FactoryBot.create(:user, site: site) }
+    let(:expected_status) { 201 }
+
+    let(:request_params) do
+      {
+        'name' => new_name,
+        'email' => new_email,
+        'phone' => new_phone,
+        'message' => new_message
+      }
+    end
+
+    it 'records who created a model' do
+      request_page
+
+      message = Message.last
+      expect(message.versions.last.whodunnit).to eq request_user.id.to_s
     end
   end
 end
