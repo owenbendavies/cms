a109971386aeeedb34a5b42505d82b47ace4d3b6
Owen Davies <git@obduk.com>
2018-03-09 18:59:53 +0000

    Refactor email specs and footer copyright

---
+++ spec/mailers/application_mailer_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationMailer do
+  subject(:email) { CustomDeviseMailer.confirmation_instructions(user, token) }
+
+  let(:site) { FactoryBot.create(:site) }
+  let(:user) { FactoryBot.create(:user, site: site) }
+  let(:token) { rand(10_000) }
+
+  it 'has from name as site name' do
+    addresses = email.header['from'].address_list.addresses
+    expect(addresses.first.display_name).to eq site.name
+  end
+
+  it 'has site copyright in body' do
+    expect(email.body).to have_content "#{site.name} © #{Time.zone.now.year}"
+  end
+
+  describe 'links' do
+    context 'with ssl enabled' do
+      let(:environment_variables) { { DISABLE_SSL: nil } }
+
+      it 'has https links' do
+        expect(email.body).to have_link 'Confirm Email', href: %r{\Ahttps://#{site.host}/}
+      end
+    end
+
+    context 'with ssl disabled' do
+      it 'has http links' do
+        expect(email.body).to have_link 'Confirm Email', href: %r{\Ahttp://#{site.host}/}
+      end
+    end
+  end
+
+  describe 'charity number' do
+    context 'with site with charity number' do
+      let(:site) { FactoryBot.create(:site, charity_number: new_number) }
+
+      it 'has charity number in body' do
+        expect(email.body).to have_content "Registered charity number #{new_number}"
+      end
+    end
+
+    context 'with site without charity number' do
+      it 'does not have charity number in body' do
+        expect(email.body).not_to have_content 'Registered charity'
+      end
+    end
+  end
+end
--- spec/support/shared_examples/email_for_site.rb
+++ spec/support/shared_examples/email_for_site.rb
@@ -3,40 +3,15 @@ RSpec.shared_examples 'email for site' do
     expect(email.from).to eq [site.email]
   end
 
-  it 'has from name as site name' do
-    addresses = email.header['from'].address_list.addresses
-    expect(addresses.first.display_name).to eq site.name
+  it 'has message subject' do
+    expect(email.subject).to eq expected_subject
   end
 
   it 'has site name in body' do
     expect(email.body).to have_content site.name
   end
 
-  context 'with site with copyright' do
-    before { site.update!(copyright: new_name) }
-
-    it 'has copyright in body' do
-      expect(email.body).to have_content "#{new_name} © #{Time.zone.now.year}"
-    end
-  end
-
-  context 'with site without copyright' do
-    it 'has site name copyright in body' do
-      expect(email.body).to have_content "#{site.name} © #{Time.zone.now.year}"
-    end
-  end
-
-  context 'with site with charity number' do
-    before { site.update!(charity_number: new_number) }
-
-    it 'has charity number in body' do
-      expect(email.body).to have_content "Registered charity number #{new_number}"
-    end
-  end
-
-  context 'with site with charity number' do
-    it 'does not have charity number in body' do
-      expect(email.body).not_to have_content 'Registered charity'
-    end
+  it 'has body' do
+    expect(email.body).to have_content expected_body
   end
 end
