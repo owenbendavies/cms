8c8c405c9e7d22121b6038f837b16f4b30e05f6d
Owen Davies <git@obduk.com>
2018-05-10 07:11:06 +0100

    Change from sprokets to webpacker for all assets

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,17 +3,19 @@ SecureHeaders::Configuration.default do |config|
 
   asset_src = ["'self'", "'unsafe-inline'", ENV['ASSET_HOST']].compact
 
+  script_src = asset_src + [
+    'https://www.google-analytics.com',
+    'https://cdnjs.cloudflare.com'
+  ]
+
   config.csp = {
     preserve_schemes: true,
     default_src: ["'none'"],
     child_src: ["'self'"],
     connect_src: ["'self'", 'https://api.rollbar.com'],
-    font_src: ["'self'", 'https:'],
+    font_src: ["'self'", 'https:', 'data:'],
     img_src: ["'self'", 'https:', 'data:'],
-    script_src: asset_src + [
-      'https://www.google-analytics.com',
-      'https://cdnjs.cloudflare.com'
-    ],
+    script_src: script_src,
     style_src: asset_src
   }
 end
