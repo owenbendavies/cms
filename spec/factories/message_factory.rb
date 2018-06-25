22c297010ec09ccbf104f42ad593215b8cfed09b
Owen Davies <git@obduk.com>
2018-06-25 16:08:01 +0100

    Remove annotate gem

--- spec/factories/message_factory.rb
+++ spec/factories/message_factory.rb
@@ -1,29 +1,3 @@
-# == Schema Information
-#
-# Table name: messages
-#
-#  id                    :integer          not null, primary key
-#  site_id               :integer          not null
-#  name                  :string(64)       not null
-#  email                 :string(64)       not null
-#  phone                 :string(32)
-#  message               :text             not null
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
-#  uid                   :string           not null
-#  privacy_policy_agreed :boolean          default(FALSE)
-#
-# Indexes
-#
-#  index_messages_on_created_at  (created_at)
-#  index_messages_on_site_id     (site_id)
-#  index_messages_on_uid         (uid) UNIQUE
-#
-# Foreign Keys
-#
-#  fk_messages_site_id  (site_id => sites.id)
-#
-
 FactoryBot.define do
   factory :message do
     site
