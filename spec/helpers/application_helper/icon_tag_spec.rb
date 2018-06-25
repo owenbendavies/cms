2acde450b199027e8d139c7d39f86d64498f7c92
Owen Davies <git@obduk.com>
2018-06-25 20:39:37 +0100

    Merge application_helper specs

--- spec/helpers/application_helper/icon_tag_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationHelper, '#icon_tag' do
-  it 'renders an icon' do
-    expect(helper.icon_tag('fas fa-icon fa-fw')).to eq '<i class="fas fa-icon fa-fw"></i>'
-  end
-end
