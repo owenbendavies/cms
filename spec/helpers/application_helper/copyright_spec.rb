f98defcf15594a0a86882b00b19d0bf9de3d4046
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2018-06-25 20:39:37 +0100

    Merge application_helper specs

--- spec/helpers/application_helper/copyright_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationHelper, '#copyright' do
-  let(:site) { FactoryBot.build_stubbed(:site) }
-
-  it 'uses site name' do
-    expect(copyright(site)).to eq "#{site.name} © #{Time.zone.now.year}"
-  end
-end
