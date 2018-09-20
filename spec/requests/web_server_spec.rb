be26740f99ade6f0ddad51d48888b1d5ebe8803b
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2018-09-20 19:12:41 +0100

    Remove unneeded shared examples

--- spec/requests/web_server_spec.rb
+++ spec/requests/web_server_spec.rb
@@ -1,12 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe 'Application web server' do
-  include_context 'with headers'
-
   let(:request_method) { :get }
   let(:request_path) { '/sitemap' }
 
-  shared_examples 'asset headers' do
+  context 'when visiting an html page' do
+    let(:random_headers) { %w[Content-Length ETag Set-Cookie X-Request-Id X-Runtime] }
+
+    let(:non_random_headers) { response.headers.except(*random_headers) }
+
+    let(:asset_src) { "'self' 'unsafe-inline' http://localhost:37511" }
+
+    let(:script_src) do
+      [
+        asset_src,
+        'https://www.google-analytics.com',
+        'https://cdnjs.cloudflare.com'
+      ].join(' ')
+    end
+
+    let(:csp_header) do
+      [
+        "default-src 'none'",
+        "child-src 'self'",
+        "connect-src 'self' https://api.rollbar.com",
+        "font-src 'self' https: data:",
+        "img-src 'self' https: data:",
+        "script-src #{script_src}",
+        "style-src #{asset_src}"
+      ].join('; ')
+    end
+
+    let(:expected_non_random_headers) do
+      {
+        'Cache-Control' => 'max-age=0, private, must-revalidate',
+        'Content-Security-Policy' => csp_header,
+        'Content-Type' => 'text/html; charset=utf-8',
+        'Referrer-Policy' => 'strict-origin-when-cross-origin',
+        'Vary' => 'Accept-Encoding',
+        'X-Content-Type-Options' => 'nosniff',
+        'X-Download-Options' => 'noopen',
+        'X-Frame-Options' => 'DENY',
+        'X-Permitted-Cross-Domain-Policies' => 'none',
+        'X-XSS-Protection' => '1; mode=block'
+      }
+    end
+
+    let(:expected_headers) { random_headers + expected_non_random_headers.keys }
+
+    it 'sets only expected headers' do
+      request_page
+      expect(response.headers.keys).to contain_exactly(*expected_headers)
+    end
+
+    it 'sets correct values for headers' do
+      request_page
+      expect(non_random_headers).to eq expected_non_random_headers
+    end
+  end
+
+  context 'when visiting a asset' do
+    let(:request_path) do
+      Dir
+        .glob(Rails.root.join('public', '*', 'application-*.js'))
+        .first
+        .gsub(Rails.root.join('public').to_s, '')
+    end
+
     before { request_page }
 
     it 'sets CORS allowed method' do
@@ -24,23 +84,6 @@ RSpec.describe 'Application web server' do
     it 'sets Cache Control' do
       expect(response.headers['Cache-Control']).to eq "public, max-age=#{1.year.to_i}"
     end
-  end
-
-  context 'when visiting an html page' do
-    include_examples 'sets headers' do
-      let(:expected_content_type) { 'text/html; charset=utf-8' }
-    end
-  end
-
-  context 'when visiting a asset' do
-    let(:request_path) do
-      Dir
-        .glob(Rails.root.join('public', '*', 'application-*.js'))
-        .first
-        .gsub(Rails.root.join('public').to_s, '')
-    end
-
-    include_examples 'asset headers'
   end
 
   context 'with bad client' do
