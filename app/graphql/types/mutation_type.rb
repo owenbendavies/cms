02be300a0bb3add594eb144e5baef8aa86c1be21
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2018-11-30 21:23:14 +0000

    Add GraphQL update site

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < BaseObject
     field :delete_messages, mutation: Mutations::DeleteMessages
     field :delete_pages, mutation: Mutations::DeletePages
+    field :update_site, mutation: Mutations::UpdateSite
   end
 end
