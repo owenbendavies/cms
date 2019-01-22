04a316fd0921fcd0c0cc7e3f5197a62e6e264773
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-01-22 19:08:59 +0000

    Fix vscode debugger

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+unless ENV['DISABLE_BOOTSNAP'] == 'true'
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
