1dfae1c922db77ab35a75262af7f9399af3e4606
Owen Ben Davies <git@obduk.com>
2019-02-20 21:16:37 +0000

    Change from cloudformation to Terraform

---
+++ terraform/app_environment/modules/aws_iam/outputs.tf
@@ -0,0 +1,9 @@
+output "access_key_id" {
+  sensitive = true
+  value     = "${aws_iam_access_key.app.id}"
+}
+
+output "secret_access_key" {
+  sensitive = true
+  value     = "${aws_iam_access_key.app.secret}"
+}
