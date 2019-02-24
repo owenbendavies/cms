bd820151d2e72e69c6fc34af7c1dabb72ed2b578
Owen Ben Davies <git@obduk.com>
2019-02-24 19:38:43 +0000

    Fix terraform Heroku config

--- terraform/app_environment/modules/heroku/variables.tf
+++ terraform/app_environment/modules/heroku/variables.tf
@@ -1,10 +1,10 @@
 variable "app_name" {}
 variable "aws_access_key_id" {}
 variable "aws_cloudfront_domain_name" {}
-variable "aws_cognito_arn" {}
 variable "aws_cognito_client_id" {}
 variable "aws_cognito_client_secret" {}
 variable "aws_cognito_domain" {}
+variable "aws_cognito_id" {}
 variable "aws_region" {}
 variable "aws_s3_assets_bucket_name" {}
 variable "aws_secret_access_key" {}
