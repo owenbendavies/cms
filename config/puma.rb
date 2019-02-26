3df50817abc0417bef3e40719875cf13f779f30f
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-02-26 18:29:43 +0000

    Reconnect database on puma boot

--- config/puma.rb
+++ config/puma.rb
@@ -30,5 +30,9 @@ workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
 #
 preload_app!
 
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
