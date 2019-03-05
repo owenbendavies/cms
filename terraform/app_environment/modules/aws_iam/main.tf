9d53108defb9b3dbc3962cb581d7caaa5287c9ae
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-03-05 20:00:52 +0000

    Refactors to Terraform

--- terraform/app_environment/modules/aws_iam/main.tf
+++ terraform/app_environment/modules/aws_iam/main.tf
@@ -6,60 +6,20 @@ resource "aws_iam_access_key" "app" {
   user = "${aws_iam_user.app.name}"
 }
 
-data "aws_iam_policy_document" "app_cognito" {
-  statement {
-    effect    = "Allow"
-    resources = ["${var.aws_cognito_arn}"]
-
-    actions = [
-      "cognito-idp:ListUsersInGroup",
-    ]
-  }
-}
-
 resource "aws_iam_user_policy" "app_cognito" {
   name   = "cognito"
-  policy = "${data.aws_iam_policy_document.app_cognito.json}"
+  policy = "${data.aws_iam_policy_document.cognito.json}"
   user   = "${aws_iam_user.app.name}"
-}
-
-data "aws_iam_policy_document" "app_s3" {
-  statement {
-    actions   = ["s3:ListBucket"]
-    effect    = "Allow"
-    resources = ["${var.aws_s3_assets_bucket_arn}"]
-  }
-
-  statement {
-    effect    = "Allow"
-    resources = ["${var.aws_s3_assets_bucket_arn}/*"]
-
-    actions = [
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:DeleteObject",
-    ]
-  }
 }
 
 resource "aws_iam_user_policy" "app_s3" {
   name   = "s3"
-  policy = "${data.aws_iam_policy_document.app_s3.json}"
+  policy = "${data.aws_iam_policy_document.s3.json}"
   user   = "${aws_iam_user.app.name}"
-}
-
-data "aws_iam_policy_document" "app_ses" {
-  statement {
-    actions   = ["ses:SendRawEmail"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
 }
 
 resource "aws_iam_user_policy" "app_ses" {
   name   = "ses"
-  policy = "${data.aws_iam_policy_document.app_ses.json}"
+  policy = "${data.aws_iam_policy_document.ses.json}"
   user   = "${aws_iam_user.app.name}"
 }
---
+++ terraform/app_environment/providers.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  version = "1.57"
+  region  = "eu-west-1"
+}
+
+provider "heroku" {
+  version = "1.7.4"
+}
