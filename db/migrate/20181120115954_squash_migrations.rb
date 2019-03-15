d2a85b9ad28feb89cbbfff2d5ca5173b6790cd10
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-03-15 22:06:25 +0000

    Squash migrations

--- db/migrate/20181120115954_squash_migrations.rb
+++ db/migrate/20181120115954_squash_migrations.rb
@@ -53,20 +53,10 @@ class SquashMigrations < ActiveRecord::Migration[5.2]
       t.integer "main_menu_position"
       t.text "custom_html"
       t.boolean "hidden", default: false, null: false
+      t.string "uid", null: false
       t.index ["site_id", "main_menu_position"], name: "index_pages_on_site_id_and_main_menu_position", unique: true
       t.index ["site_id", "url"], name: "index_pages_on_site_id_and_url", unique: true
       t.index ["site_id"], name: "index_pages_on_site_id"
-    end
-
-    create_table "site_settings", id: :serial, force: :cascade do |t|
-      t.integer "user_id", null: false
-      t.integer "site_id", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.boolean "admin", default: false, null: false
-      t.index ["site_id"], name: "index_site_settings_on_site_id"
-      t.index ["user_id", "site_id"], name: "index_site_settings_on_user_id_and_site_id", unique: true
-      t.index ["user_id"], name: "index_site_settings_on_user_id"
     end
 
     create_table "sites", id: :serial, force: :cascade do |t|
@@ -74,7 +64,6 @@ class SquashMigrations < ActiveRecord::Migration[5.2]
       t.string "name", limit: 64, null: false
       t.string "google_analytics", limit: 32
       t.string "charity_number", limit: 32
-      t.string "stylesheet_filename", limit: 40
       t.text "sidebar_html_content"
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
@@ -83,54 +72,10 @@ class SquashMigrations < ActiveRecord::Migration[5.2]
       t.jsonb "links", default: []
       t.integer "privacy_policy_page_id"
       t.string "uid", null: false
+      t.string "email", null: false
+      t.text "css"
       t.index ["host"], name: "index_sites_on_host", unique: true
-      t.index ["stylesheet_filename"], name: "index_sites_on_stylesheet_filename", unique: true
-    end
-
-    create_table "stylesheets", id: :serial, force: :cascade do |t|
-      t.integer "site_id", null: false
-      t.text "css", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["site_id"], name: "index_stylesheets_on_site_id", unique: true
-    end
-
-    create_table "users", id: :serial, force: :cascade do |t|
-      t.string "email", limit: 64, null: false
-      t.string "encrypted_password", limit: 64
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "sign_in_count", default: 0, null: false
-      t.datetime "current_sign_in_at"
-      t.datetime "last_sign_in_at"
-      t.inet "current_sign_in_ip"
-      t.inet "last_sign_in_ip"
-      t.integer "failed_attempts", default: 0, null: false
-      t.string "unlock_token"
-      t.datetime "locked_at"
-      t.datetime "remember_created_at"
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.string "confirmation_token"
-      t.datetime "confirmed_at"
-      t.datetime "confirmation_sent_at"
-      t.string "unconfirmed_email"
-      t.boolean "sysadmin", default: false, null: false
-      t.string "name", limit: 64, null: false
-      t.string "invitation_token"
-      t.datetime "invitation_created_at"
-      t.datetime "invitation_sent_at"
-      t.datetime "invitation_accepted_at"
-      t.integer "invited_by_id"
-      t.string "google_uid"
-      t.string "uid", null: false
-      t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-      t.index ["email"], name: "index_users_on_email", unique: true
-      t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-      t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-      t.index ["uid"], name: "index_users_on_uid", unique: true
-      t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+      t.index ["uid"], name: "index_sites_on_uid", unique: true
     end
 
     create_table "versions", id: :serial, force: :cascade do |t|
@@ -147,10 +92,6 @@ class SquashMigrations < ActiveRecord::Migration[5.2]
     add_foreign_key "images", "sites", name: "fk_images_site_id"
     add_foreign_key "messages", "sites", name: "fk_messages_site_id"
     add_foreign_key "pages", "sites", name: "fk_pages_site_id"
-    add_foreign_key "site_settings", "sites", name: "fk_site_settings_site_id"
-    add_foreign_key "site_settings", "users", name: "fk_site_settings_user_id"
     add_foreign_key "sites", "pages", column: "privacy_policy_page_id", name: "fk__sites_privacy_policy_page_id"
-    add_foreign_key "stylesheets", "sites", name: "fk_stylesheets_site_id"
-    add_foreign_key "users", "users", column: "invited_by_id", name: "fk_users_invited_by_id"
   end
 end
