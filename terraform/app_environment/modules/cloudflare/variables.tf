9bb8a0d46b251f0ca7b67e948b1c3ababecb6173
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-03-20 06:53:23 +0000

    Set up CloudFlare via Terraform

---
+++ terraform/app_environment/modules/cloudflare/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {}
+variable "dmarc_record" {}
+variable "heroku_domain" {}
+
+variable "gsuite_domainkeys" {
+  type = "list"
+}
+
+variable "gsuite_domains" {
+  type = "list"
+}
+
+variable "mailchip_domains" {
+  type = "list"
+}
+
+variable "root_domains" {
+  type = "list"
+}
+
+variable "ses_dkim_tokens" {
+  type = "list"
+}
+
+variable "ses_mail_from_domains" {
+  type = "list"
+}
+
+variable "ses_verification_tokens" {
+  type = "list"
+}
