d560f8e41efa5b7e30bcc24de81153f697e7ba18
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-03-26 07:46:07 +0000

    Add Google StackDriver notifications

---
+++ terraform/app_environment/modules/google_monitoring/main.tf
@@ -0,0 +1,31 @@
+resource "google_monitoring_alert_policy" "main" {
+  combiner              = "AND"
+  count                 = "${length(var.domains)}"
+  display_name          = "${var.domains[count.index]}"
+  notification_channels = ["${google_monitoring_notification_channel.main.name}"]
+
+  conditions {
+    display_name = "${var.domains[count.index]}"
+  }
+}
+
+resource "google_monitoring_notification_channel" "main" {
+  display_name = "${var.notification_email}"
+  type         = "email"
+
+  labels = {
+    email_address = "${var.notification_email}"
+  }
+}
+
+resource "google_monitoring_uptime_check_config" "main" {
+  count        = "${length(var.domains)}"
+  display_name = "${var.domains[count.index]}"
+  period       = "60s"
+  timeout      = "10s"
+
+  http_check {
+    path    = "/home"
+    use_ssl = true
+  }
+}
