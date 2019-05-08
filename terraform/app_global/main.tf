74139e22daac921b3ee260b190e2240c2c331070
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-05-08 20:04:50 +0100

    Remove AWS Config and Cloudtrail as now done via aws-account

--- terraform/app_global/main.tf
+++ terraform/app_global/main.tf
@@ -8,26 +8,6 @@ terraform {
   }
 }
 
-module "aws_cloudtrail" {
-  source = "./modules/aws_cloudtrail"
-
-  app_name                = "${local.app_name}"
-  aws_s3_logs_bucket_name = "${module.aws_s3.logs_bucket_name}"
-}
-
-module "aws_config" {
-  source = "./modules/aws_config"
-
-  app_name                = "${local.app_name}"
-  aws_s3_logs_bucket_name = "${module.aws_s3.logs_bucket_name}"
-}
-
-module "aws_s3" {
-  source = "./modules/aws_s3"
-
-  app_name = "${local.app_name}"
-}
-
 module "heroku_pipeline" {
   source = "./modules/heroku_pipeline"
 
