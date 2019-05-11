9b3c5a87ae815e264f9655a639570425bfab7b43
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-05-11 09:05:15 +0100

    Remove old terraform providers

--- terraform/app_global/providers.tf
+++ terraform/app_global/providers.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  region  = "us-east-1"
-  version = "1.57"
-}
-
-provider "aws" {
-  alias   = "eu-west-1"
-  region  = "eu-west-1"
-  version = "1.57"
-}
-
 provider "heroku" {
   version = "1.7.4"
 }
