cbeab9790f8165d1a44e6e4112f7679719a31780
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-06-27 19:22:58 +0100

    Simplify CSS endpoint

--- app/controllers/css_controller.rb
+++ app/controllers/css_controller.rb
@@ -1,16 +1,18 @@
 class CssController < ApplicationController
-  skip_before_action :render_site_not_found, only: %i[show]
   skip_before_action :authenticate_user!, only: %i[show]
   skip_after_action :verify_authorized, only: %i[show]
 
   def show
-    id = params[:id].rpartition('-').first
-    site = Site.where(id: id).where.not(css: nil).take!
+    if @site.css
+      expires_in 1.year, public: true
 
-    expires_in 1.year, public: true
-
-    respond_to do |format|
-      format.css { render plain: site.css }
+      respond_to do |format|
+        format.css { render plain: @site.css }
+      end
+    else
+      respond_to do |format|
+        format.css { head 404 }
+      end
     end
   end
 end
