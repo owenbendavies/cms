3311ceb7f49103fe1e5cd937cba091c01a2f6d87
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-08-01 22:03:44 +0100

    Add images to admin

---
+++ spec/features/admin/images_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin images' do
+  context 'with multiple images' do
+    let!(:images) do
+      ('a'..'k').map do |i|
+        FactoryBot.create(:image, name: "Image #{i}", site: site)
+      end
+    end
+
+    before do
+      login_as site_user
+      visit '/home'
+      click_link 'Admin'
+      click_link 'Images'
+    end
+
+    scenario 'clicking pagination' do
+      expect(all('img').size).to eq 10
+
+      expect(page).to have_css "img[alt='#{images.first.name}']"
+      expect(page).not_to have_css "img[alt='#{images.last.name}']"
+
+      click_button 'Next'
+
+      expect(page).not_to have_css "img[alt='#{images.first.name}']"
+      expect(page).to have_css "img[alt='#{images.last.name}']"
+
+      click_button 'Prev'
+
+      expect(page).to have_css "img[alt='#{images.first.name}']"
+      expect(page).not_to have_css "img[alt='#{images.last.name}']"
+    end
+  end
+end
