a08d0627e3f4283a01c7b4282eb41b3f14eb5879
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-08-01 22:03:44 +0100

    Add images to admin

---
+++ app/webpacker/admin/routes/images/ImageList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+
+import { List } from 'react-admin';
+
+const ImageGrid = ({ ids, data }) => (
+  <GridList cellHeight="auto" cols={4}>
+    {ids.map(id => (
+      <GridListTile key={id}>
+        <img src={data[id].url} alt={data[id].name} />
+      </GridListTile>
+    ))}
+  </GridList>
+);
+
+export const ImageList = props => (
+  <List {...props}>
+    <ImageGrid />
+  </List>
+);
