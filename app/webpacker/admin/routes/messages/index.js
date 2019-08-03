a986092929d0399c5d102260f2817ab48b7fd0bf
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-08-03 09:43:16 +0100

    Refactor JS

---
+++ app/webpacker/admin/routes/messages/index.js
@@ -0,0 +1,11 @@
+import MessageIcon from '@material-ui/icons/Email';
+
+import { MessageList } from './MessageList';
+import { MessageShow } from './MessageShow';
+
+export const messageOptions = {
+  icon: MessageIcon,
+  list: MessageList,
+  name: 'messages',
+  show: MessageShow,
+};
