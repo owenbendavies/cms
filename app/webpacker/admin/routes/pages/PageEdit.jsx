2d3c1b7853ce98760c853475fbd03db993ac616b
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-08-26 20:40:31 +0100

    Add admin edit pages

--- app/webpacker/admin/routes/pages/PageEdit.jsx
+++ app/webpacker/admin/routes/pages/PageEdit.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 
+import RichTextInput from 'ra-input-rich-text';
+
 import {
   BooleanInput,
   DisabledInput,
   Edit,
-  SimpleForm,
+  FormTab,
+  TabbedForm,
   TextInput,
 } from 'react-admin';
 
 const PageTitle = ({ record }) => <span>{`Page ${record.name}`}</span>;
 
+const pageToolbar = [
+  [
+    { header: [2, 3, false] },
+    'bold',
+    'italic',
+    { script: 'sub' },
+    { script: 'super' },
+  ],
+  [{ list: 'bullet' }, { list: 'ordered' }],
+  ['link'],
+  ['clean'],
+];
+
 export const PageEdit = props => (
   <Edit title={<PageTitle />} {...props}>
-    <SimpleForm>
-      <DisabledInput source="url" />
-      <TextInput source="name" />
-      <BooleanInput source="private" />
-      <BooleanInput source="contactForm" />
-    </SimpleForm>
+    <TabbedForm>
+      <FormTab label="content">
+        <RichTextInput source="htmlContent" toolbar={pageToolbar} />
+      </FormTab>
+      <FormTab label="Settings">
+        <DisabledInput source="url" />
+        <TextInput source="name" />
+        <BooleanInput source="private" />
+        <BooleanInput source="contactForm" />
+      </FormTab>
+    </TabbedForm>
   </Edit>
 );
