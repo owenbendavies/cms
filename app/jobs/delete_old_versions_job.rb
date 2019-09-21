939cd005f58617277f6e50c6674c3c01ab0d4a9f
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-09-21 14:04:58 +0100

    Don't delete old messages

---
+++ app/jobs/delete_old_versions_job.rb
@@ -0,0 +1,11 @@
+class DeleteOldVersionsJob < ApplicationJob
+  def perform
+    old_models = PaperTrail::Version.where(
+      'created_at < ?', 30.days.ago
+    ).where.not(item_type: 'Page')
+
+    return unless old_models.count.positive?
+
+    old_models.delete_all
+  end
+end
