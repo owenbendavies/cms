a6e63d2fa17ad1969af1782c08eeaed6aca95453
Owen Ben Davies <734943+obduk@users.noreply.github.com>
2019-09-21 14:04:58 +0100

    Don't delete old messages

--- app/jobs/daily_job.rb
+++ app/jobs/daily_job.rb
@@ -1,7 +1,7 @@
 class DailyJob < ApplicationJob
   def perform
     CleanS3Job.perform_later
-    DeleteOldModelsJob.perform_later
+    DeleteOldVersionsJob.perform_later
     ValidateDataJob.perform_later
   end
 end
