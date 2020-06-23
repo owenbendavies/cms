9e30976c0e8baca84732f0e85c22e2a957b471e0
Owen Ben Davies <734943+owenbendavies@users.noreply.github.com>
2020-06-23 17:46:15 +0100

    Remove all terraform providers

--- terraform/app_environment/providers.tf
+++ terraform/app_environment/providers.tf
@@ -3,14 +3,6 @@ provider "aws" {
   version = "2.18.0"
 }
 
-provider "cloudflare" {
-  version = "1.16.1"
-}
-
-provider "datadog" {
-  version = "2.0.2"
-}
-
 provider "heroku" {
   version = "2.0.1"
 }
