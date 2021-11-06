3e617b3155ead993f7ab61458285b5f19c0f6ee6
Owen Ben Davies <734943+owenbendavies@users.noreply.github.com>
2021-11-06 10:15:45 +0000

    Remove references to master

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,13 @@ module Cms
       generator.orm :active_record, primary_key_type: :uuid
     end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
