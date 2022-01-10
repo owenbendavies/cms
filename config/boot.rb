45e0e1e40b66834402fd8f1de331c18138480e88
Owen Ben Davies <734943+owenbendavies@users.noreply.github.com>
2022-01-10 14:58:02 +0000

    Remove spring

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-unless ENV['DISABLE_BOOTSNAP'] == 'true'
-  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-end
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
--- config/spring.rb
+++
@@ -1,1 +0,0 @@
-Spring.watch('.ruby-version', '.rbenv-vars', 'tmp/restart.txt', 'tmp/caching-dev.txt')
