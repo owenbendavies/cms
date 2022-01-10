bd3911968e9eab71479081d199d907828a30b89e
Owen Ben Davies <734943+owenbendavies@users.noreply.github.com>
2022-01-10 09:26:43 +0000

    Upgrade Rails config from 6 to 7

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
@@ -27,8 +30,6 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
-  config.assets.compile = true
 
   # Don't care if the mailer can't send.
   # config.action_mailer.raise_delivery_errors = false
