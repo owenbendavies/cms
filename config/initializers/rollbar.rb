8baee203bc2306305b1afdc4597838a59ad66ccc
Owen Ben Davies <734943+owenbendavies@users.noreply.github.com>
2022-05-16 10:11:22 +0100

    Fix rubocop issues

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
 
   # Here we'll disable in 'test':
   config.enabled = ENV['ROLLBAR_ACCESS_TOKEN'].present?
@@ -51,5 +51,5 @@ Rollbar.configure do |config|
   # environment variable like this: `ROLLBAR_ENV=staging`. This is a recommended
   # setup for Heroku. See:
   # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
-  config.environment = ENV['ROLLBAR_ENV'] || Rails.env
+  config.environment = ENV.fetch('ROLLBAR_ENV') { Rails.env }
 end
